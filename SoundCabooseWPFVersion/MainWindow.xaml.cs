cf318bea81c846fe22e6d273236eb419e432bf15
Legonois <wesley.baldwin@outlook.com>
2022-06-25 18:36:44 -0500

    Working on theme

--- SoundCabooseWPFVersion/MainWindow.xaml.cs
+++ SoundCabooseWPFVersion/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using Syncfusion;
+using Syncfusion.Windows.Tools.Controls;
 
 namespace SoundCabooseWPFVersion
 {
@@ -21,9 +23,28 @@ namespace SoundCabooseWPFVersion
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int iClicked;
+
         public MainWindow()
         {
             InitializeComponent();
+            //DockingManager dockingManager = new DockingManager();
+
+            //ContentControl SolutionExplorer = new ContentControl();
+            //ContentControl ToolBox = new ContentControl();
+            //ContentControl Properties = new ContentControl();
+            //ContentControl Output = new ContentControl();
+            //ContentControl StartPage = new ContentControl();
+
+            ////Add content controls as child of DockingManager
+
+            //dockingManager.Children.Add(SolutionExplorer);
+            //dockingManager.Children.Add(ToolBox);
+            //dockingManager.Children.Add(Properties);
+            //dockingManager.Children.Add(Output);
+            //dockingManager.Children.Add(StartPage);
+
+            //this.Content = dockingManager;
         }
 
         private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
